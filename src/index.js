b4b8d118b4f35746f616ed146b5c6bc1c53ca38a
Egor <lalalayes2@gmail.com>
2020-09-29 16:57:26 +0300

    feat: solved typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if (array === 0 || array.length === 0)
+    return 0;
+  return array.reduce((min, current) => {
+    return current < min ? current: min;
+  });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if (array === 0 || array.length === 0)
+    return 0;
+  return array.reduce((max, current) => {
+    return current > max ? current: max;
+  });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if (array === 0 || array.length === 0)
+    return 0;
+  return array.reduce((sum, current) => sum + current, 0)/array.length
 }
